eb905a52351305b63701a2f2f58e7a24787e9905
VladV98 <50819289+VladV98@users.noreply.github.com>
2020-03-04 16:03:40 +0200

    npm i

---
+++ julia-tthk-TARge-gv18-NodeJS-EJS-Templating – koopia/routes/admin.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const rootDirectory = require('../utilities/path');
+const router = express.Router();
+const products = [];
+
+
+router.get('/add-product', function(req, res){
+    //res.sendFile(path.join(rootDirectory, 'views', 'add-product.html'))
+    res.render('add-product', {
+        pageTitle: "Add New Product",
+        path: "/admin/add-product"
+    
+    
+    });
+});
+
+router.post('/add-product', function(req, res){
+    console.log(req.body.title);
+    products.push({title: req.body.title});
+    res.redirect('/');
+});
+
+//module.exports = router;
+exports.router = router;
+exports.products = products;
